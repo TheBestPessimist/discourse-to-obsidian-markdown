c34c57e018c09446e1ffbc7ba2f3be49523ccf98
TheBestPessimist <cristian@tbp.land>
2025-02-16 13:33:00 +0200

    feat: Add Discourse to Markdown processing

---
+++ src/main/kotlin/land/tbp/discourse/to/markdown/2/discourse-to-markdown-processing/Discourse to Markdown Processing.kt
@@ -0,0 +1,3 @@
+package land.tbp.discourse.to.markdown.`2`.`discourse-to-markdown-processing`
+
+// read and deserialize using the available serialization library the files from the object `Dump` AI!
