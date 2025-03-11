c05a91c4000af65bd70a800849cde839a4c418bd
TheBestPessimist <cristian@tbp.land>
2025-03-11 10:50:19 +0200

    Create the post with raw markdown

--- src/main/kotlin/land/tbp/discourse/to/markdown/ObsidianDto.kt
+++ src/main/kotlin/land/tbp/discourse/to/markdown/ObsidianDto.kt
@@ -1,26 +1,26 @@
 package land.tbp.discourse.to.markdown
 
+import kotlinx.serialization.Serializable
 import java.time.Instant
 
 // This is the thing i will write in a file
+@Serializable
 data class Topic(
     val categoryName: String,
-    val tags: List<String>, // todo
+    val tags: List<String>,
     val title: String,
     val slug: String,
-    val createdAt: Instant = Instant.now(),// todo
+    @Serializable(with = InstantSerializer::class)
+    val createdAt: Instant = Instant.now(),
     val posts: List<Post>,
-    // todo
-    // should be something like:
-    // https://discourse.tbp.land/t/all-tomorrows-a-billion-year-chronicle-of-the-myriad-species-and-varying-fortunes-of-man-nemo-ramjet/25
-    // so that my linking history is not lost!
-    // basically, i should make it such that all posts inside my discourse are available under the **same** address in the future
-    // val slugs: List<String>,
+    val fullUrls: List<String>,
 )
 
 
+@Serializable
 data class Post(
     val rawMarkdown: String,
     val id: Int,
-    val createdAt: Instant = Instant.now(), // todo
+    @Serializable(with = InstantSerializer::class)
+    val createdAt: Instant = Instant.now(),
 )
--- src/main/kotlin/land/tbp/discourse/to/markdown/processing/2. Discourse to Markdown Processing.kt
+++ src/main/kotlin/land/tbp/discourse/to/markdown/processing/2. Discourse to Markdown Processing.kt
@@ -34,51 +34,32 @@ fun main() {
     - belongs to category
     - has a list of tags
      */
-    val topics = buildList<Topic> {
-
-
+    val topics = buildList {
         for ((id, jsonElem) in DumpReader.topicInfo()) {
-
-            if (id != 4692) continue
+            // if (id != 4692) continue
 
             val categoryId = jsonElem.jsonObject["category_id"]?.jsonPrimitive?.int
             val categoryName = DumpReader.categories().categoryList.categories.single { it.id == categoryId }.name
             val tags = jsonElem.jsonObject["tags"]!!.jsonArray.map { it.jsonPrimitive.content }
-
             val title = jsonElem.jsonObject["title"]!!.jsonPrimitive.content
             // fancy title is the url encoded version. not needed
             // val fancyTitle = jsonElem.jsonObject["fancy_title"]!!.jsonPrimitive.content
 
             val slug = jsonElem.jsonObject["slug"]!!.jsonPrimitive.content
+            val fullUrls = listOf(
+                "https://discourse.tbp.land/t/$id",
+                "https://discourse.tbp.land/t/$slug/$id",
+            )
             val createdAt = jsonElem.jsonObject["created_at"]!!.jsonPrimitive.content.let { Instant.parse(it) }
 
             // todo imageURL, thumbnails might be interesting fields???
 
-            val fullUrl1 = "https://discourse.tbp.land/t/$id"
-            println(fullUrl1)
-
-            val fullUrl2 = "https://discourse.tbp.land/t/$slug/$id"
-            println(fullUrl2)
-
-            // val posts
-            //
-            // add(
-            //     Topic(
-            //         categoryName,
-            //         tags,
-            //         title,
-            //         slug,
-            //         createdAt,
-            //         posts,
-            //     )
-            // )
+            val posts = getPosts(id, jsonElem)
+            add(Topic(categoryName, tags, title, slug, createdAt, posts, fullUrls))
         }
     }
 
-
-    // DumpReader.topicInfo()[4678]
-    //     .also { println(it) }
-    //
+    println(json.encodeToString(topics))
 
 
     // val allTopics: MutableList<Topic> = Collections.synchronizedList(mutableListOf<Topic>())
@@ -88,4 +69,20 @@ fun main() {
     //     println(allTopics.size)
     //     val t = allTopics.sortedWith(compareBy({ it.categoryName }, { it.title }))
     //     Files.writeString(Path("./zzzzzzzz.txt"), t.joinToString("\n".repeat(10)))
+}
+
+private fun getPosts(id: Int, jsonElem: JsonElement): List<Post> {
+    val postIds = jsonElem.jsonObject["post_stream"]!!.jsonObject["stream"]!!.jsonArray.map { it.jsonPrimitive.int }
+    val topicPosts = DumpReader.topicPosts()[id]!!
+    val posts = postIds.map { postId ->
+        val post = topicPosts.single { it.jsonObject["id"]!!.jsonPrimitive.int == postId }.jsonObject
+        val raw = post["raw"]!!.jsonPrimitive.content
+        val createdAt = post["created_at"]!!.jsonPrimitive.content.let { Instant.parse(it) }
+        Post(
+            raw,
+            postId,
+            createdAt
+        )
+    }
+    return posts
 }
