6312175ff8587429c59bfaacfe48a71e9a69faac
TheBestPessimist <cristian@tbp.land>
2025-03-11 13:34:13 +0200

    Dont print in `Base 62 Decoder`

--- src/main/kotlin/land/tbp/discourse/to/markdown/processing/xx. Image Converter.kt
+++ src/main/kotlin/land/tbp/discourse/to/markdown/processing/xx. Image Converter.kt
@@ -6,8 +6,8 @@ import java.sql.SQLOutput
 
 // this is how i convert the base62 hashes to the actual image hash url
 fun main() {
-    println(base62Decode("6LX9Y8dW88pEjXU4ufdLkTrkhc0") == "2f78c1ac988dd200abdba00c2ef28678eb0650dc")
-    println(base62Decode("o3t8U3Df0Nrf0D2fcmX1PEDmTfJ") == "a89897c22b40a070068d7a395ab18e6a545197ab")
+    println(base62Decode("6LX9Y8dW88pEjXU4ufdLkTrkhc0").also(::println) == "2f78c1ac988dd200abdba00c2ef28678eb0650dc")
+    println(base62Decode("o3t8U3Df0Nrf0D2fcmX1PEDmTfJ").also(::println) == "a89897c22b40a070068d7a395ab18e6a545197ab")
 }
 
 
@@ -16,6 +16,5 @@ fun base62Decode(uploadUrlHash: String): String {
     val b = Base62.createInstanceWithInvertedCharacterSet()
     val d = b.decode(uploadUrlHash.encodeToByteArray())
     val decoded = d.toHexString(HexFormat.Default)
-    println(decoded)
     return decoded
 }
