6333845125b3e2feb598d91a9c425d322fbb5d11
TheBestPessimist <cristian@tbp.land>
2025-03-25 14:16:45 +0200

    todo

---
+++ README.md
@@ -0,0 +1,12 @@
+todo
+- [ ] fix base62 decoding this one:
+```
+UploadUrl(fullMatch=![eb10d828f8f1a4fe4a33761d9ff93cd227eca623](upload://bIHzpmIohIJFFGCqxLDlWXZbNC7.),
+altText=eb10d828f8f1a4fe4a33761d9ff93cd227eca623,
+ uploadId=bIHzpmIohIJFFGCqxLDlWXZbNC7.)
+
+```
+- [ ] make the blog importer smart.
+  - example post: 4409 
+  - [ ] the last 2 comments of a topic have the link to the original blog post and
+  - [ ] the tags of that blog post
--- src/main/kotlin/land/tbp/discourse/to/markdown/processing/xx. Image Converter.kt
+++ src/main/kotlin/land/tbp/discourse/to/markdown/processing/xx. Image Converter.kt
@@ -7,6 +7,10 @@ import io.seruco.encoding.base62.Base62
 fun main() {
     println(base62Decode("6LX9Y8dW88pEjXU4ufdLkTrkhc0").also(::println) == "2f78c1ac988dd200abdba00c2ef28678eb0650dc")
     println(base62Decode("o3t8U3Df0Nrf0D2fcmX1PEDmTfJ").also(::println) == "a89897c22b40a070068d7a395ab18e6a545197ab")
+    println(base62Decode("bIHzpmIohIJFFGCqxLDlWXZbNC7").also(::println) == "a89897c22b40a070068d7a395ab18e6a545197ab")
+
+
+
 }
 
 
@@ -17,3 +21,13 @@ fun base62Decode(uploadUrlHash: String): String {
     val decoded = d.toHexString(HexFormat.Default)
     return decoded
 }
+
+
+/*
+this one fails:
+
+UploadUrl(fullMatch=![eb10d828f8f1a4fe4a33761d9ff93cd227eca623](upload://bIHzpmIohIJFFGCqxLDlWXZbNC7.),
+altText=eb10d828f8f1a4fe4a33761d9ff93cd227eca623,
+ uploadId=bIHzpmIohIJFFGCqxLDlWXZbNC7.)
+
+ */
